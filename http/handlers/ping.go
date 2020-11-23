788f23fd04817464d852f47ddeb77722c68d0c45
Akshit Garg <garg.akshit@gmail.com>
2020-11-23 23:45:48 +0530

    [Handlers] Fix the PingHandler

--- http/handlers/ping.go
+++ http/handlers/ping.go
@@ -4,5 +4,5 @@ import "github.com/gofiber/fiber/v2"
 
 // PingHandler is retuns a plain response "OK" if all the middleware based checks pass
 func PingHandler(ctx *fiber.Ctx) error {
-	return ctx.Send("OK")
+	return ctx.SendString("OK")
 }
